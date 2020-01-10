e56d0e8acc995f5979563cd218cda32b86a3a5a5
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-01-10 11:36:09 +0100

    style: format navbar menu item

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,33 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">About the company <span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-uppercase" href="#">About the company </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Investments</a>
+                        <a class="nav-link text-uppercase" href="#">Investments</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Career</a>
+                        <a class="nav-link text-uppercase" href="#">Career</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Reference</a>
+                        <a class="nav-link text-uppercase" href="#">Reference</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Reviews</a>
+                        <a class="nav-link text-uppercase" href="#">Reviews</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contacts</a>
+                        <a class="nav-link text-uppercase" href="#">Contacts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase" href="#">Log In</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
